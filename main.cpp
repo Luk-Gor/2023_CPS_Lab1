b9fdf0f55c8a85777bffe672310450a430a24870
Luk-Gor <lgorecki275@gmail.com>
2023-03-12 12:19:58 +0000

    Software Update
    Signed-off-by: Luk-Gor <lgorecki275@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "mbed.h"
+#include "BufferedSerial.h"
 
-Serial pc(USBTX, USBTX);
+//Serial pc(USBTX, USBTX);
+
+BufferedSerial pc(USBTX, USBRX);
 
 //counter
 int i=2;
@@ -13,16 +16,16 @@ int i_2 = 1;
 
 int main()
 {
-    pc.printf("Using simple loops (%d) \r\n", n);
-    pc.printf("%d, ", i_1);
-    pc.printf("%d, ", i_2);
+    printf("Using simple loops (%d) \r\n", n);
+    printf("%d, ", i_1);
+    printf("%d, ", i_2);
 
     while(i<n){
         int sum = i_1 + i_2;
         i_1 = i_2;
         i_2 = sum;
         i++;
-        pc.printf("%d, ", sum);
+        printf("%d, ", sum);
     }
 
 
